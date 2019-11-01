3749c6fb2d61558ebac2b44075fde8765f2bfa21
Eric Rogers <Eric.Rogers@noaa.gov>
2019-11-01 23:03:28 +0000

    updated ex-scripts

--- scripts/exregional_cleanup.sh
+++ scripts/exregional_cleanup.sh
@@ -10,16 +10,18 @@ set -x
 
 # Remove temporary working directories
 cd ${STMP}
-if [ $RUN = fv3sar ]; then
+if [ $RUN = fv3sar -o $RUN = fv3sar_da ]; then
   cd tmpnwprd
 elif [ $RUN = fv3nest ]; then
   cd tmpnwprd_nest
 fi
 
-rm -rf regional_make_bc_${dom}_${CDATE}
-rm -rf regional_make_ic_${dom}_${CDATE}
-rm -rf regional_forecast_tm00_${dom}_${CDATE}
-rm -rf regional_post_${dom}_f*_${CDATE}
+rm -rf regional_make_bc_*_${CDATE}
+rm -rf regional_make_ic_*_${CDATE}
+rm -rf regional_forecast_firstguess_${dom}_${CDATE}
+rm -rf regional_forecast_tm*_${dom}_${CDATE}
+rm -rf regional_gsianl_tm*_${dom}_${CDATE}
+rm -rf regional_post_*_${dom}_${CDATE}
 rm -rf regional_post_goes_${dom}_f*_${CDATE}
 
 exit
