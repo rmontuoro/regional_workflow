69ce2edff54097d6b763b6ac0254a6adcd90cef4
Eric Rogers <Eric.Rogers@noaa.gov>
2019-11-01 23:05:57 +0000

    updated rocoto files with consistent job names

--- rocoto/old/run_fv3sar_da.sh
+++ rocoto/old/run_fv3sar_da.sh
@@ -4,7 +4,7 @@ set +x
 . /apps/lmod/lmod/init/sh
 set -x
 
-. /scratch2/NCEPDEV/fv3-cam/Eric.Rogers/regional_workflow/rocoto/machine-setup.sh
+. /scratch2/NCEPDEV/fv3-cam/Eric.Rogers/regional_workflow_hera_da/rocoto/machine-setup.sh
 export machine=${target}
 
 module use ${HOMEfv3}/modulefiles/${machine}
@@ -13,4 +13,4 @@ module load fv3
 module use /apps/modules/modulefiles
 module load rocoto/1.3.1
 
-rocotorun -v 10 -w /scratch2/NCEPDEV/fv3-cam/Eric.Rogers/regional_workflow/rocoto/old/drive_fv3sar_da.xml -d /scratch2/NCEPDEV/fv3-cam/Eric.Rogers/regional_workflow/rocoto/old/drive_fv3sar_da.db
+rocotorun -v 10 -w /scratch2/NCEPDEV/fv3-cam/Eric.Rogers/regional_workflow_hera_da/rocoto/old/drive_fv3sar_da.xml -d /scratch2/NCEPDEV/fv3-cam/Eric.Rogers/regional_workflow_hera_da/rocoto/old/drive_fv3sar_da.db
